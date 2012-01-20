86c9f53f7d3dad980d8301bb90b4bac424008f7b
Scott Davis <jetviper21@gmail.com>
2012-01-20 18:03:59 -0500

    forgot some files

---
+++ test/integrations/rails_32_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+class Rails33Test < Test::Unit::TestCase
+  include CompassRails::Test::RailsHelpers
+
+  def test_rails_app_created
+    within_rails_app('test_railtie', RAILS_3_2) do |project|
+      project.install_compass
+      assert project.has_gem? 'compass'
+      project.bundle
+      assert project.rails3?
+    end
+  end
+
+end
