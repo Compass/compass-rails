e51c59d3800116cc5de27c2b392a9aa06c583274
Scott Davis <jetviper21@gmail.com>
2012-01-28 14:16:34 -0500

    added template for rails 2.3 generator

---
+++ lib/generators/compass/install.rb
@@ -0,0 +1,8 @@
+class CompassRails::Generators::Base < Rails::Generator::NamedBase
+class InstallGenerator < CompassRails::Generators::Base
+  def manifest
+    record do |m|
+      # Do something
+    end
+  end
+end
