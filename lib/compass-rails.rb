6a617c0b812c43d1145b60ec7b86c6ad8e2da618
Chris Eppstein <chris@eppsteins.net>
2012-03-18 16:11:14 -0700

    Only if there are sass_options

--- lib/compass-rails.rb
+++ lib/compass-rails.rb
@@ -190,8 +190,10 @@ module CompassRails
       }.each do |compass_option, sass_option|
         set_maybe sass_config, compass_config, sass_option, compass_option
       end
-      compass_config.sass_options.each do |config, value|
-        sass_config.send("#{config}=", value)
+      if compass_config.sass_options
+        compass_config.sass_options.each do |config, value|
+          sass_config.send("#{config}=", value)
+        end
       end
     end
 
