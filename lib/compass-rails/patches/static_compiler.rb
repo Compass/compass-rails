1749c06f15dc4b058427e7969810457213647fb8
Andrey A.I. Sitnik <andrey@sitnik.ru>
2013-01-14 21:50:23 +0100

    Update to reflect changes in Sprockets
    See ai/compass-rails@9d8069d54a66a3bcf20e8c7ebdccde681104a6af

--- lib/compass-rails/patches/static_compiler.rb
+++ lib/compass-rails/patches/static_compiler.rb
@@ -1,14 +1,30 @@
 if CompassRails.rails4?
-  require 'sprockets/rails/static_compiler'
   module Sprockets
-    module Rails
-      class StaticCompiler
-        cattr_accessor :generated_sprites
-        self.generated_sprites = {}
-        def write_manifest_with_sprites(manifest)
-          write_manifest_without_sprites(manifest.merge(self.class.generated_sprites))
-        end
-        alias_method_chain :write_manifest, :sprites
+    class SassTemplate < Tilt::Template
+      def evaluate(context, locals, &block)
+        # Use custom importer that knows about Sprockets Caching
+        cache_store = SassCacheStore.new(context.environment)
+        paths  = context.environment.paths.map { |path| SassImporter.new(context, path) }
+        paths += ::Rails.application.config.sass.load_paths
+
+        options = {
+          :filename => eval_file,
+          :line => line,
+          :syntax => syntax,
+          :cache_store => cache_store,
+          :importer => SassImporter.new(context, context.pathname),
+          :load_paths => paths,
+          :sprockets => {
+            :context => context,
+            :environment => context.environment
+          }
+        }
+
+        ::Sass::Engine.new(data, options).render
+      rescue ::Sass::SyntaxError => e
+        # Annotates exception message with parse line number
+        context.__LINE__ = e.sass_backtrace.first[:line]
+        raise e
       end
     end
   end
