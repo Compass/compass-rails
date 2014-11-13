6ee4d8600df7be482f2044df2f25856f93da2e01
Dimko <deemox@gmail.com>
2014-11-13 23:18:12 +0300

    Fixed cache busting for Rails 4

--- lib/compass-rails/patches/3_1.rb
+++ lib/compass-rails/patches/3_1.rb
@@ -2,7 +2,7 @@ require 'compass-rails/patches/compass'
 require 'compass-rails/patches/static_compiler'
 
 Compass::Core::SassExtensions::Functions::Urls::GeneratedImageUrl.module_eval do
-  def generated_image_url(path, only_path = nil)
-    asset_url(path, Sass::Script::String.new("image"))
+  def generated_image_url(path, cache_buster = Sass::Script::Bool.new(false))
+    asset_url(path, Sass::Script::String.new('image'))
   end
 end
