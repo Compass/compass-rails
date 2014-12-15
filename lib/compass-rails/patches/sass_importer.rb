09f73daf98bed35dcb29999431cbe4175aa9e86f
Daniel P. Clark <6ftdan@gmail.com>
2014-12-14 21:12:12 -0500

    Support new SassImporter method signature
    Fixes #195

--- lib/compass-rails/patches/sass_importer.rb
+++ lib/compass-rails/patches/sass_importer.rb
@@ -7,9 +7,10 @@ end
 klass.class_eval do
   def evaluate(context, locals, &block)
     # Use custom importer that knows about Sprockets Caching
-    cache_store = Sprockets::SassCacheStore.new(context.environment)
+    cache_store = begin Sprockets::SassCacheStore.new(context.environment); rescue; nil; end ||
+        Sprockets::SassCacheStore.new(context.environment, "1")
     paths  = context.environment.paths.map { |path| CompassRails::SpriteImporter.new(context, path) }
-    paths += context.environment.paths.map { |path| self.class.parent::SassImporter.new(context, path) }
+    paths += context.environment.paths.map { |path| sass_importer(context, path) }
     paths += ::Rails.application.config.sass.load_paths
 
 
@@ -18,7 +19,7 @@ klass.class_eval do
       :line => line,
       :syntax => syntax,
       :cache_store => cache_store,
-      :importer => self.class.parent::SassImporter.new(context, context.pathname),
+      :importer => sass_importer(context, context.pathname),
       :load_paths => paths,
       :sprockets => {
         :context => context,
@@ -31,6 +32,12 @@ klass.class_eval do
     # Annotates exception message with parse line number
     context.__LINE__ = e.sass_backtrace.first[:line]
     raise e
+  end
+
+  private
+  def sass_importer(context, path)
+    begin self.class.parent::SassImporter.new(context, path); rescue; nil; end ||
+        self.class.parent::SassImporter.new(path)
   end
 end    
 
