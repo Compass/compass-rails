f5e3049e8c9ef714145d880f07dc6e0066d69324
Craig McNamara <craig@caring.com>
2015-01-04 23:54:11 -0800

    Revert "Merge pull request #213 from Compass/fix-jruby"
    JRuby fixed jruby:2423 in https://github.com/jruby/jruby/commit/2eb920db33f25c9caad656b23abaf55256ceee4f

--- test/helpers/file_helper.rb
+++ test/helpers/file_helper.rb
@@ -11,7 +11,7 @@ module CompassRails
 
       def rm(path)
         debug "Removing: #{path}"
-        ::FileUtils.rm(path, force: true)
+        ::FileUtils.rm(path)
         assert !File.file?(path), "rm: #{path} failed"
       end
 
