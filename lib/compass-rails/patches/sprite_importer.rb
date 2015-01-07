555ea920302fda409b96285f5c7ad3a7a35b36d8
Sean Walbran <swalbran@socialcast.com>
2015-01-07 15:11:09 -0600

    cleanup no-longer-needed attr_reader, move context assignment outside loop

--- lib/compass-rails/patches/sprite_importer.rb
+++ lib/compass-rails/patches/sprite_importer.rb
@@ -3,7 +3,7 @@ require 'compass/sprite_importer'
 
 module CompassRails
   class SpriteImporter < Compass::SpriteImporter
-    attr_reader :context, :root
+    attr_reader :root
 
     def initialize(root)
       @root = root
@@ -11,10 +11,10 @@ module CompassRails
 
     def find(uri, options)
       if old = super(uri, options)
+        context = options[:sprockets][:context]
         self.class.files(uri).each do |file|
           relative_path = Pathname.new(file).relative_path_from(Pathname.new(root))
           begin
-            context = options[:sprockets][:context]
             pathname = context.resolve(relative_path)
             context.depend_on_asset(pathname)
           rescue Sprockets::FileNotFound
