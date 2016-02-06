d53c1fa17dd9ebaf33f2491388e4cf92bd81a445
Vojta Drbohlav <vojta.d@gmail.com>
2016-02-06 20:14:18 +0100

    Fixed image_path_for_size to work properly with sprockets >= 3.
    Use ::Rails.application.assets_manifest instead of
    ::Rails.application.assets when config.assets.compile is not set.

--- lib/compass-rails/patches/compass.rb
+++ lib/compass-rails/patches/compass.rb
@@ -2,11 +2,23 @@ Compass::Core::SassExtensions::Functions::ImageSize.class_eval do
   private
 
   def image_path_for_size(image_file)
-    begin
-      file = CompassRails.sprockets.find_asset(image_file)
-      return file
-    rescue ::Sprockets::FileOutsidePaths
-      return super(image_file)
+    if ::Rails.application.config.assets.compile
+      begin
+        file = CompassRails.sprockets.find_asset(image_file)
+        return file.respond_to?(:pathname) ? file.pathname.to_s : file
+      rescue ::Sprockets::FileOutsidePaths
+        return super(image_file)
+      end
+    else
+      return image_file if File.exists?(image_file)
+
+      assets_manifest = ::Rails.application.assets_manifest
+      file = assets_manifest.assets[image_file]
+      if file
+        ::Rails.root.join("public", assets_manifest.directory, file)
+      else
+        super(image_file)
+      end
     end
   end
 end
